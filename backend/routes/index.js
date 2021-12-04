730ce41b1c4ebf760f40c84555153b4005c23da7
Corbin <corbinarcus@hotmail.com>
2021-12-04 13:16:10 -0500

    phase 0 done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world'), function(req,res){
+router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!')
-}
+  res.send('Hello World!');
+});
 
 
 module.exports = router;
