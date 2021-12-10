718077d91551724542904980277f8b99acec84dc
Corbin <corbinarcus@hotmail.com>
2021-12-10 13:09:52 -0500

    this is taking too long

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
 const { Group } = require('../../db/models')
+const { User } = require('../../db/models/user')
 
 const router = express.Router();
 
@@ -20,10 +21,10 @@ router.get(
   '/:id(\\d+)',
   asyncHandler(async(req, res) => {
     const groupId = req.params.id
-    const group = await Group.findByPk(groupId, { include:
-      [{
-      model: User
-      }]
+    const group = await Group.findByPk(groupId, {
+      include: {
+        models: User
+      }
     })
     res.json({
       group
