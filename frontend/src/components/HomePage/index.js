84206fe353441e342e16d63896bf58cf129844f2
Corbin <corbinarcus@hotmail.com>
2021-12-13 00:10:23 -0500

    done

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,11 +2,17 @@ import React from 'react'
 import styles from '../../css-modules/HomePage.module.css'
 
 
+
 function HomePage() {
+
+
   return (
-    <div className={styles.div}>
-      <h1 className={styles.welcome}>Welcome to LGS Locater!</h1>
-    </div>
+    <>
+      <div className={styles.div}>
+        <h1 className={styles.welcome}>Welcome to LGS Locater!</h1>
+      </div>
+      <footer className={styles.footer}><a className={styles.link} href='https://github.com/Corbin-Arcus'>Check out my Github page!</a></footer>
+    </>
   )
 }
 
